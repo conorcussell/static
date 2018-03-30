be9675f45ccc02f88fb2b1170582234515f8b75e
Conor Cussell <contact@conorcussell.co.uk>
2018-03-30 14:11:14 +0100

    Only include if we have a11y param

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ function init() {
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
   require('preact/devtools');
-  require('a11y.css/css/a11y-en.css');
+
+  if (window.location.search.includes('a11y')) {
+    require('a11y.css/css/a11y-en.css');
+  }
+
   module.hot.accept('./components/app', () => requestAnimationFrame(init));
 }
 
